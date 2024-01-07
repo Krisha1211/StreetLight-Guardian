28cff9f2fd24d7a57b4eef4116a8ec517b35b7a5
Mitul Sonagara <mitulkumarsongara@gmail.com>
2024-01-07 17:08:44 +0530

    v1

--- admin/src/routes/dashboard.js
+++ admin/src/routes/dashboard.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 const connectEnsureLogin = require('connect-ensure-login');
 
-router.get('/dashboard', connectEnsureLogin.ensureLoggedIn(), (req, res) => {
+// router.get('/dashboard', connectEnsureLogin.ensureLoggedIn(), (req, res) => {
+//     // This route is protected; only authenticated users can access it
+//     res.render('dashboard'); // Render your dashboard template or perform other actions
+// });
+
+router.get('/dashboard', (req, res) => {
     // This route is protected; only authenticated users can access it
     res.render('dashboard'); // Render your dashboard template or perform other actions
 });
